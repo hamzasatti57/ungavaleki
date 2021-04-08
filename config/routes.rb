febd4651a27d93b7fa7a2a46559c1b1b9b4f1e3e
Ali Sheikh <ali.sheikh@46labs.com>
2021-04-08 21:40:09 +0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'dashboard', to: 'home#index'
+  devise_for :users, :controllers => {
+    :sessions => "users/sessions",
+    :confirmations => "users/confirmations",
+    :passwords => "users/passwords",
+    :registrations => "users/registrations",
+    :unlocks => "users/unlocks",
+  }
+  devise_scope :user do
+    root :to => 'users/sessions#new'
+    get "sign_in", to: "users/sessions#new"
+    get "sign_up", to: "users/registrations#new"
+    get "sign_out", to: "users/sessions#destroy"
+  end
+end
